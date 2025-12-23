599ca87c1ddafc8f9940d94fadba38c813071297
YUCN\18423 <1842341952@qq.com>
2025-12-23 13:05:33 +0800

    Update UI and Logic

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,6 +1,6 @@
 // 1. 请把这里替换成你刚才 npx hardhat ignition deploy ... 获得的地址
 // 如果你找不到，重新运行 deploy 命令就能看到
-export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
+export const CONTRACT_ADDRESS = "0x340457A601B6D9d313eC7fD9A83499637eA269BE"; 
 
 // 2. 这是我们刚才写的合约的 ABI (接口定义)
 export const CONTRACT_ABI = [
--- frontend/providers.tsx
+++ frontend/providers.tsx
@@ -1,4 +1,4 @@
-'use client';
+/*'use client';
 
 import * as React from 'react';
 import { RainbowKitProvider, getDefaultWallets, getDefaultConfig } from '@rainbow-me/rainbowkit';
@@ -30,4 +30,39 @@ export function Providers({ children }: { children: React.ReactNode }) {
       </QueryClientProvider>
     </WagmiProvider>
   );
+}*/
+
+'use client';
+
+import * as React from 'react';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { WagmiProvider, http } from 'wagmi';
+import { arbitrumSepolia } from 'wagmi/chains'; // <--- 关键修改：引入 Arbitrum 测试网
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const config = getDefaultConfig({
+  appName: 'Focus Flow',
+  // 这里填你之前申请的 WalletConnect Project ID
+  projectId: '019d803c2ecde40813a5d1bf5a3d5e44', 
+  chains: [arbitrumSepolia], // <--- 关键修改：只允许连接 Arbitrum Sepolia
+  transports: {
+    [arbitrumSepolia.id]: http(), // 使用默认的公共 RPC 节点
+  },
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
+
