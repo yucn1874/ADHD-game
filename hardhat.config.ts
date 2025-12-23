d1df6605f2a17548bfac71e55880ab978d183a33
YUCN\18423 <1842341952@qq.com>
2025-12-23 13:28:27 +0800

    chore: local changes before rebase

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import type { HardhatUserConfig } from "hardhat/config";
+/*import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
 };
 
+export default config;*/
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    // Arbitrum Sepolia 测试网配置
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
 export default config;
+
