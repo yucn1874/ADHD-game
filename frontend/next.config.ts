036f33a9690d05422f4c59bfdcf57e32ac4f5429
YUCN\18423 <1842341952@qq.com>
2025-12-23 13:48:46 +0800

    chore: disable eslint during build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-
-
-
 
 export default nextConfig;
